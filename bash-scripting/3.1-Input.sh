56270de20ab66e3e601aefdfd426140e04f35154
SomayaB <somaya.bounouar@gmail.com>
2017-06-05 20:18:14 -0700

    Completes Activity 3.1

---
+++ bash-scripting/3.1-Input.sh
@@ -0,0 +1,4 @@
+# Create a simple script which will ask the user for a few pieces of information then combine this into a message which is echo'd to the screen.
+
+read -p "What is your name? " userName
+echo "Your name is $userName"
