c5048e20d8843e70c94aab8569d8de5f9e970102
SomayaB <somaya.bounouar@gmail.com>
2017-06-05 20:33:49 -0700

    Completes Activity 4.1

---
+++ bash-scripting/4.1-Arithmetic.sh
@@ -0,0 +1,6 @@
+# Create a simple script which will take two command line arguments and then multiply them together using each of the methods detailed above.
+let "a = 5 * 4"
+echo $a
+expr 6 \* 4
+b=$((7 * 4))
+echo $b
